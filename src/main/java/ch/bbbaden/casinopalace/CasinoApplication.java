28da2e527a9d64e6f37b69ef76dc1117b66a096f
Alainx277 <alainsiegrist02@gmail.com>
2019-04-04 13:56:21 +0200

    Create project

---
+++ src/main/java/ch/bbbaden/casinopalace/CasinoApplication.java
@@ -0,0 +1,11 @@
+package ch.bbbaden.casinopalace;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CasinoApplication extends Application {
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Casino Palace");
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/ch/bbbaden/casinopalace/Starter.java
@@ -0,0 +1,9 @@
+package ch.bbbaden.casinopalace;
+
+import javafx.application.Application;
+
+public class Starter {
+    public static void main(String[] args) {
+        Application.launch(CasinoApplication.class, args);
+    }
+}

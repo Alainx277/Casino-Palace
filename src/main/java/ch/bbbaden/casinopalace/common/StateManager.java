553c329d6df0d592f565071bf2fac40b35800c50
Alainx277 <alainsiegrist02@gmail.com>
2019-04-08 09:27:43 +0200

    Add state manager

---
+++ src/main/java/ch/bbbaden/casinopalace/common/StateManager.java
@@ -0,0 +1,37 @@
+package ch.bbbaden.casinopalace.common;
+
+import ch.bbbaden.casinopalace.common.states.State;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StateManager {
+    private State state;
+    private Casino casino;
+    private SceneCreator sceneCreator;
+
+    public StateManager(Casino casino, SceneCreator sceneCreator) {
+        this.casino = casino;
+        this.sceneCreator = sceneCreator;
+    }
+
+    public void transition(State state) throws IOException {
+        Stage oldStage = sceneCreator.getCurrentStage();
+        sceneCreator.createScene(state.getURL()).setStateManager(this);
+        if (oldStage != null){
+            oldStage.close();
+        }
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public Casino getCasino() {
+        return casino;
+    }
+
+    public SceneCreator getSceneCreator() {
+        return sceneCreator;
+    }
+}

978718c04bab95768e8b8f4cfc9e62c1a3caf32a
Alainx277 <alainsiegrist02@gmail.com>
2019-04-08 09:29:07 +0200

    Add ui

---
+++ src/main/java/ch/bbbaden/casinopalace/view/AdminController.java
@@ -0,0 +1,6 @@
+package ch.bbbaden.casinopalace.view;
+
+import ch.bbbaden.casinopalace.common.Controller;
+
+public class AdminController extends Controller {
+}
---
+++ src/main/java/ch/bbbaden/casinopalace/view/StatistikController.java
@@ -0,0 +1,6 @@
+package ch.bbbaden.casinopalace.view;
+
+import ch.bbbaden.casinopalace.common.Controller;
+
+public class StatistikController extends Controller {
+}

590637ab7ad0d2de7b84fd66c8d1a92786bd3ad5
Doemuu <43069409+Doemuu@users.noreply.github.com>
2019-04-08 14:15:33 +0200

    Add casino menu

--- src/main/java/ch/bbbaden/casinopalace/view/CasinoController.java
+++ src/main/java/ch/bbbaden/casinopalace/view/CasinoController.java
@@ -1,6 +1,33 @@
 package ch.bbbaden.casinopalace.view;
 
 import ch.bbbaden.casinopalace.common.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class CasinoController extends Controller {
+
+    @FXML
+    private ImageView imgViewLeft;
+    @FXML
+    private ImageView imgView3;
+    @FXML
+    private ImageView imgViewGames;
+
+    @FXML
+    private void handleLeftGame(MouseEvent event) {
+    }
+
+    @FXML
+    private void handleLeftKey(KeyEvent event) {
+    }
+
+    @FXML
+    private void handleRightGame(MouseEvent event) {
+    }
+
+    @FXML
+    private void handleGamesSelection(MouseEvent event) {
+    }
 }

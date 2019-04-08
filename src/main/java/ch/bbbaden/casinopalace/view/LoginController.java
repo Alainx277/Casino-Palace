97b26dcf267d3aedee9fc7b64c6f41e0bad94f33
GabrieleFerrali <gabriele.ferrali@stud.bbbaden.ch>
2019-04-08 11:58:57 +0200

    Update UI

--- src/main/java/ch/bbbaden/casinopalace/view/LoginController.java
+++ src/main/java/ch/bbbaden/casinopalace/view/LoginController.java
@@ -1,6 +1,41 @@
 package ch.bbbaden.casinopalace.view;
 
 import ch.bbbaden.casinopalace.common.Controller;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class LoginController extends Controller {
+
+    @FXML
+    private TextField userfield;
+    @FXML
+    private Button buttonlogin;
+    @FXML
+    private Button buttonsignin;
+
+    @FXML
+    private void clickLogin(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickSignup(ActionEvent event) throws IOException {
+        try {
+            getStateManager().getState().handleSignUp(getStateManager());
+        } catch (Exception ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
 }

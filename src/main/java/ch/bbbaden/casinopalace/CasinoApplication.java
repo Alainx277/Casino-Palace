d4b43dfccde6eed4952da7705e642dd3c8a2241e
Alainx277 <alainsiegrist02@gmail.com>
2019-04-08 09:30:46 +0200

    Open login on start

--- src/main/java/ch/bbbaden/casinopalace/CasinoApplication.java
+++ src/main/java/ch/bbbaden/casinopalace/CasinoApplication.java
@@ -1,11 +1,20 @@
 package ch.bbbaden.casinopalace;
 
+import ch.bbbaden.casinopalace.common.Casino;
+import ch.bbbaden.casinopalace.common.DatabaseStorage;
+import ch.bbbaden.casinopalace.common.SceneCreator;
+import ch.bbbaden.casinopalace.common.StateManager;
+import ch.bbbaden.casinopalace.common.states.LoginState;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 public class CasinoApplication extends Application {
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Casino Palace");
-        primaryStage.show();
+        StateManager manager = new StateManager(new Casino(new DatabaseStorage()), new SceneCreator());
+        manager.transition(new LoginState());
     }
 }

bc38ea8af8c4232ebe11edc9c73b7e0ac9f29c5b
Alain Siegrist <alainsiegrist02@gmail.com>
2019-05-05 18:00:02 +0200

    Revert "Add MockStorage for easier testing"
    This reverts commit 954711b7102cd623870d8eb358b5a9c9f49e6fb1.

--- src/main/java/ch/bbbaden/casinopalace/CasinoApplication.java
+++ src/main/java/ch/bbbaden/casinopalace/CasinoApplication.java
@@ -2,7 +2,6 @@ package ch.bbbaden.casinopalace;
 
 import ch.bbbaden.casinopalace.common.*;
 import ch.bbbaden.casinopalace.common.states.LoginState;
-import ch.bbbaden.casinopalace.common.states.PokerState;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -14,11 +13,8 @@ import java.util.logging.Logger;
 public class CasinoApplication extends Application {
     public void start(Stage primaryStage) throws Exception {
         try {
-            MockStorage storage = new MockStorage();
-            UserFactory userFactory = new UserFactory();
-            StateManager manager = new StateManager(new Casino(userFactory, storage), new SceneCreator());
-            manager.getCasino().setCurrentUser(userFactory.createUser("Bruno", "mars"));
-            manager.transition(new PokerState());
+            StateManager manager = new StateManager(new Casino(new UserFactory(), new DatabaseStorage("localhost:3306", "casino", "root", "")), new SceneCreator());
+            manager.transition(new LoginState());
         } catch (Exception e) {
             // TODO: Notify user (critical error)
             Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, e);
